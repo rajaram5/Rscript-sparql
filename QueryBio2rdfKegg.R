a55aafbd15e86df16f6b97409320b8dffe903016
Rajaram Kaliyaperumal <rr.kaliyaperumal@gmail.com>
2014-11-20 11:09:05 +0100

    first commit

---
+++ QueryBio2rdfKegg.R
@@ -0,0 +1,43 @@
+"
+@author Nirupama Benis
+@author Rajaram Kaliyaperumal
+
+@version 0.1
+@since 20-11-2014
+
+<p>
+  This rscript query kegg pathway SPARQL endpoint to retrieve the pathways associated with the given Entrez GeneID  
+</p>
+
+Input Required : Entrez GeneID
+Output : dataframe with Entrez GeneID, Pathway name and Pathway class
+"
+
+library('SPARQL')
+
+# URL of the SPARQL enpoint
+endpoint <- "http://cu.kegg.bio2rdf.org/sparql"
+
+queryStr <- "PREFIX void: <http://rdfs.org/ns/void#>
+PREFIX dv: <http://bio2rdf.org/bio2rdf.dataset_vocabulary:>
+PREFIX keggv: <http://bio2rdf.org/kegg_vocabulary:> 
+
+
+SELECT DISTINCT ?pathway (str(?pathwayName) as ?pathwayNameStr) (str(?pathwayClass) as ?pathwayClassStr)   {
+
+<http://bio2rdf.org/kegg:hsa_geneID> keggv:pathway ?pathway.
+
+
+?pathway keggv:class ?pathwayClass;
+dcterms:title ?pathwayName.
+
+}"
+
+EntrezGeneID <- "3350"
+queryStr <- gsub("geneID", EntrezGeneID, queryStr)
+
+# execute your query
+d <- SPARQL(url=endpoint, query=queryStr)
+
+# attach results to a data frame
+df <- data.frame(GeneID=EntrezGeneID, Pathwayname=d$results$pathwayNameStr, PathwayClass=factor(d$results$pathwayClassStr), stringsAsFactors=FALSE)
